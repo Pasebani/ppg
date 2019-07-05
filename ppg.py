6939676b62904a21c57081c1f8249ec901239b07
mohammad reza <52000980+Pasebani@users.noreply.github.com>
2019-07-05 21:04:41 +0430

    Delete ppg.py

--- ppg.py
+++
@@ -1,9 +0,0 @@
-import random, os, string
-os.system("cls" or "clear")
-item = 1
-i = int(input("How Many Want You:"))
-character = string.printable
-while i >= item:
-    a = ''.join(random.sample(character,8))
-    print(" Password {0} is :{1}".format(item,a))
-    item += 1
